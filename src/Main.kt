2369fdad04f2e7aafbce0146a991e9521aad1761
waimea-gkidd <gekidd@waimea.school.nz>
2025-02-25 14:27:32 +1300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -32,7 +32,7 @@ fun main() {
     println(snacks)
 
     // Add in new items in specific places
-    snacks.add(1, "Bacon Scratching")
+    snacks.add(1, "Bacon Scratchings")
 
     // Sort the list
     snacks.sort()
@@ -42,10 +42,34 @@ fun main() {
     val item = snacks.random()
     println(item)
 
+    // Shuffle the list (randomise it)
+    snacks.shuffle()
+    println("shuffled: $snacks")
+
     // Loop though the list
     for (item in snacks) {
 
     }
+    // Search for an item
+    println("Cranberries is in the list: ${ snacks.contains("Cranberries") }")
+    println("Twiglets is in the list: ${ snacks.contains("Twiglets") }")
+
+    // Getting index of an item
+    println("Cranberries index: ${snacks.indexOf("Cranberries")}")
+    println("Twiglets index: ${snacks.indexOf("Twiglets")}")
+
+    // Loop through the list
+    for (Snack in snacks) {
+        println(Snack)
+    }
+
+    // Loop using an index
+    for (i in 0 ..< snacks.size) {
+        println("$i: ${ snacks[i] }")
+    }
+    // Loop getting index and values
+    for ((i, snack) in snacks.withIndex()) {
+        println("$i: $snack")
+    }
 
 }
-
